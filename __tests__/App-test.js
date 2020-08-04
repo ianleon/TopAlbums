7719b7aaa9b15107a29e57c8009c8d36a87a68b9
Ian Leon <iannleon@me.com>
2020-08-03 21:36:31 -0400

    Adds some basic tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,37 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import DetailView from '../DetailView';
+import AlbumsView from '../AlbumsView';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders details correctly', () => {
+
+	let testRoute = {
+		"params": {
+			"item": {
+				key: "0",
+				position: "1",
+				image: "http://example.com",
+				title: "Test",
+				artist: "Test",
+				category: "TestMusic",
+				link: "http://example.com",
+				releaseDate: "Test"
+			}
+		}
+	};
+  	renderer.create(<DetailView route={testRoute} />);
+});
+
+it('renders top albums correctly', () => {
+
+	let testNavigation = {
+		navigation: {
+			navigate: () => {}
+		}
+	};
+  	renderer.create(<AlbumsView navigation={testNavigation} fetch="{}"/>);
 });

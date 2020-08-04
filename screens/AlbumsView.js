57c039527364c71619325ead2971fa9a1e1bb791
Ian Leon <iannleon@me.com>
2020-08-04 12:06:51 -0400

    Formatting the AlbumsView code

--- screens/AlbumsView.js
+++ screens/AlbumsView.js
@@ -6,24 +6,12 @@
  */
 import React, { Component } from 'react'
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Button,
-  Text,
-  UIManager,
-  LayoutAnimation,
-  StatusBar,
-  FlatList,
-  Image,
-  Linking,
-  TextInput,
-  Pressable
-} from 'react-native';
-
+  SafeAreaView, StyleSheet, ScrollView,
+  View, Button, Text, UIManager,
+  LayoutAnimation, StatusBar, FlatList,
+  Image, Linking, TextInput, Pressable
+} from 'react-native'
 import filter from 'lodash.filter'
-
 import { Avatar, Badge, Icon, withBadge } from 'react-native-elements'
 
 UIManager.setLayoutAnimationEnabledExperimental &&
@@ -31,35 +19,31 @@ UIManager.setLayoutAnimationEnabledExperimental(true);
 
 class ListRow extends Component {
 	render = () => {
-
 		const {data : {item}, navigate} = this.props;
-
-		return (<SafeAreaView>
-			<Pressable
-			onPress={() => navigate('AlbumDetails', {item})}
-			style={styles.listRows}>
-			<View>
-			  <Avatar
-			    rounded
-			    source={{
-			      uri: item.image,
-			    }}
-			    size="medium"
-				containerStyle={{ margin: 10 }}
-			  />
-
-
-			  <Badge
-			    status="error"
-				value={item.position}
-			    containerStyle={{ position: 'absolute', top: 4, right: -4 }}
-			  />
-			</View>
-
-
-				<Text style={styles.listTitles}>{item.title}</Text>
-			</Pressable>
-		</SafeAreaView>)
+		return (
+      <SafeAreaView>
+        <Pressable
+          onPress={() => navigate('AlbumDetails', {item})}
+          style={styles.listRows}>
+          <View>
+            <Avatar
+              rounded
+              source={{ uri: item.image }}
+              size="medium"
+              containerStyle={{ margin: 10 }} />
+            <Badge
+              status="error"
+              value={item.position}
+              containerStyle={{
+                position: 'absolute',
+                top: 4,
+                right: -4
+              }} />
+          </View>
+          <Text style={styles.listTitles}>{item.title}</Text>
+        </Pressable>
+      </SafeAreaView>
+    )
 	}
 }
 
@@ -73,7 +57,10 @@ class AlbumsView extends Component {
 
 	contains = ({ title, artist }, query) => {
 
-		if (title.toLowerCase().includes(query) || artist.toLowerCase().includes(query)) {
+		if (
+			title.toLowerCase().includes(query) ||
+			artist.toLowerCase().includes(query)
+		) {
 			return true;
 	  	}
 
@@ -82,9 +69,12 @@ class AlbumsView extends Component {
 
 	handleSearch = text => {
 		const formattedQuery = text.toLowerCase();
-		const filteredTopAlbums = filter(this.state.topAlbums, album => {
-			return this.contains(album, formattedQuery)
-		});
+		const filteredTopAlbums = filter(
+      this.state.topAlbums,
+      album => {
+        return this.contains(album, formattedQuery)
+  		}
+    );
 		this.setState({ filteredTopAlbums, query: text });
 	}
 
@@ -100,73 +90,73 @@ class AlbumsView extends Component {
 	};
 
   	componentDidMount = () => {
+      fetch("https://itunes.apple.com/us/rss/topalbums/limit=100/json")
+        .then(response => response.json())
+        .then(musicData => {
+          let cleanData = musicData.feed.entry.map((ent,i) => {
 
-		fetch("https://itunes.apple.com/us/rss/topalbums/limit=100/json")
-		.then(response => response.json())
-		.then(musicData => {
+            let key = i + "";
+            let position = i+1;
+            let title = ent["im:name"].label;
+            let artist = ent["im:artist"].label;
+            let releaseDate = ent["im:releaseDate"].attributes.label;
+            let category = ent.category.attributes.label;
+            let image = ent["im:image"][0].label;
+            let link = ent.link.attributes.href;
 
-		  let cleanData = musicData.feed.entry.map((ent,i) => {
-
-			  let key = i + "";
-			  let position = i+1;
-			  let title = ent["im:name"].label;
-
-			  let artist = ent["im:artist"].label;
-			  let releaseDate = ent["im:releaseDate"].attributes.label;
-			  let category = ent.category.attributes.label;
-			  let image = ent["im:image"][0].label;
-			  let link = ent.link.attributes.href;
-
-			  return ({
-				  key, position, image, releaseDate,
-				  title, artist,category, link});
-		  });
-		  this.setAnimation();
-		  this.setState({topAlbums: cleanData, filteredTopAlbums: cleanData});
-
-		});
+    			  return (
+              {
+      				  key, position, image, releaseDate,
+      				  title, artist,category, link
+              }
+            );
+          });
+          this.setAnimation();
+          this.setState({topAlbums: cleanData, filteredTopAlbums: cleanData});
+        });
     }
 
 	renderHeader = () => (
-	  <View
-	    style={{
-	      backgroundColor: '#fff',
-	      padding: 10,
-	      alignItems: 'center',
-	      justifyContent: 'center'
-	    }}>
+    <View style={styles.listHeader}>
 	    <TextInput
-		  style={styles.listSearch}
-	      autoCapitalize='none'
+		    style={styles.listSearch}
 	      onChangeText={this.handleSearch}
-	      placeholder='Search'
-	    />
+	      autoCapitalize='none'
+	      placeholder='Search' />
 	  </View>
 	)
 
 	render = () => {
+
 		const { navigate } = this.props.navigation;
+
 		return (<FlatList
 			ListHeaderComponent={this.renderHeader}
 			data={this.state.filteredTopAlbums}
-			renderItem={(item) => <ListRow data={item} navigate={navigate} /> } />);
+			renderItem={ (item) => <ListRow data={item} navigate={navigate} /> } />);
 	}
 }
 
 const styles = StyleSheet.create({
+  listHeader: {
+    backgroundColor: '#fff',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   listSearch: {
-	height: 50,
-	fontSize: 20,
-	borderRadius: 10,
-	borderColor: '#ddd',
-	borderWidth: 1,
-	width: "100%",
-	paddingLeft: 10
+  	height: 50,
+  	fontSize: 20,
+  	borderRadius: 10,
+  	borderColor: '#ddd',
+  	borderWidth: 1,
+  	width: "100%",
+  	paddingLeft: 10
   },
   listTitles: {
-	padding: 10,
-	fontSize: 20,
-	flex: 1
+  	padding: 10,
+  	fontSize: 20,
+  	flex: 1
   },
   listImage: {
 	  width: 70,
